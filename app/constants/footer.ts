323f18c0e01b9510fc1b567adb3a06b45b94a323
Empi9245 <emilparlog01@gmail.com>
2026-05-22 21:24:44 +0200

    feat: adapt all texts and content for Exeva agency

--- app/constants/footer.ts
+++ app/constants/footer.ts
@@ -3,32 +3,26 @@ import { FooterLink } from "../types";
 export const FOOTER_LINKS: FooterLink[] = [
   {
     name: 'LinkedIn',
-    hoverText: 'Connect with me',
+    hoverText: 'Seguici su LinkedIn',
     icon: 'icons/linkedin.svg',
-    url: 'https://www.linkedin.com/in/mohit-virli-4780b6112/',
+    url: 'https://www.linkedin.com/company/exeva',
   },
   {
     name: 'GitHub',
-    hoverText: 'Open Sourcing',
+    hoverText: 'I nostri progetti',
     icon: 'icons/github.svg',
-    url: 'https://github.com/mohitvirli',
-  },
-  {
-    name: 'Spotify',
-    hoverText: 'Curated playlists',
-    icon: 'icons/spotify.svg',
-    url: 'https://open.spotify.com/user/21hr4w2hzp4ceidewwsb4bxoy',
+    url: 'https://github.com/Empi9245',
   },
   {
     name: 'Instagram',
-    hoverText: '@clevirli',
+    hoverText: '@exeva.agency',
     icon: 'icons/instagram.svg',
-    url: 'https://www.instagram.com/clevirli/',
+    url: 'https://www.instagram.com/exeva.agency/',
   },
   {
-    name: 'Resume',
-    hoverText: 'Download',
+    name: 'Contatti',
+    hoverText: 'Scrivici',
     icon: 'icons/file.svg',
-    url: './Mohit Virli - Resume.pdf',
+    url: 'mailto:info@exeva.it',
   }
-];
+]
